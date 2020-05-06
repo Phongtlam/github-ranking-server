4c8eea6fcaed899308a1b2851ccd984457fa64e0
Phongtlam <Phongtlam@gmail.com>
2020-05-06 12:50:00 -0700

    tests

--- test/utils/getTotalPageFromLinks.spec.js
+++ test/utils/getTotalPageFromLinks.spec.js
@@ -1,8 +1,9 @@
-const { expect, test } = require("@jest/globals");
+const { expect, test } = require('@jest/globals');
 const getTotalPageFromLink = require('../../utils/getTotalPageFromLink.js');
 
 test('return correct max page count', () => {
-  const link = '<https://api.github.com/organizations/913567/repos?page=2&sort=updated>; rel="next", <https://api.github.com/organizations/913567/repos?page=6&sort=updated>; rel="last"';
+  const link =
+    '<https://api.github.com/organizations/913567/repos?page=2&sort=updated>; rel="next", <https://api.github.com/organizations/913567/repos?page=6&sort=updated>; rel="last"';
   expect(getTotalPageFromLink(link)).toBe(6);
 });
 
