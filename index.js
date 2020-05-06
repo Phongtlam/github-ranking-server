92037fbb48c5e256d3f17a9a43e60dc93d7c1b20
Phongtlam <Phongtlam@gmail.com>
2020-05-05 23:47:48 -0700

    change default server address

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const app = require('./app');
 const server = require('http').createServer(app);
 
-const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, HOSTNAME, () => {
-  console.log(`Github rank is listening at http://${HOSTNAME}:${PORT}/!`);
+server.listen(PORT, () => {
+  console.log(`Github rank is listening at port ${PORT}!`);
 });
